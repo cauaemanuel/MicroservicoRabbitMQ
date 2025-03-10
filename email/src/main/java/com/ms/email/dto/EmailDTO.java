f49e499e758d54a192d93587a2f778772c4577d5
caua <emanullcaua8@gmail.com>
2025-03-10 11:36:44 -0300

    configuracao do consumer e rabbitMQConfig

---
+++ email/src/main/java/com/ms/email/dto/EmailDTO.java
@@ -0,0 +1,9 @@
+package com.ms.email.dto;
+
+import java.util.UUID;
+
+public record EmailDTO(UUID userId,
+                       String emailTo,
+                       String subject,
+                       String text) {
+}
